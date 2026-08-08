aa23828668b7f55404113ec79f623643b3dca56f
Swostika Balami <swostikabalami111@gmail.com>
2026-08-08 16:53:55 +0545

    about section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import { lazy, Suspense } from "react";
 
 const Hero = lazy(() => import("../components/sections/Hero"));
+const AboutSection = lazy( () => import("../components/sections/about/AboutSection")
+);
 const Services = lazy(() => import("../components/sections/Services"));
 
 interface HomeProps {
@@ -11,7 +13,7 @@ function Home({ docked }: HomeProps) {
   return (
     <Suspense fallback={null}>
       <Hero docked={docked} />
-
+ <AboutSection />
       <Services />
     </Suspense>
   );
